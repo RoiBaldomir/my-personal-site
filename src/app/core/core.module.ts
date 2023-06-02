9b4ac263276600ee6a678b3b2c594ad7587401b1
Roi Baldomir <92852224+RoiBaldomir@users.noreply.github.com>
2023-06-02 16:59:04 +0200

    Add header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent
   ]
 })
 export class CoreModule { }
