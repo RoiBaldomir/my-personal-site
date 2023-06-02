3ae5e4dda152800442bceb6b05b7170baff221c0
Roi Baldomir <92852224+RoiBaldomir@users.noreply.github.com>
2023-06-02 17:03:50 +0200

    Add about me section

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutMeComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutMeComponent
   ]
 })
 export class CoreModule { }
